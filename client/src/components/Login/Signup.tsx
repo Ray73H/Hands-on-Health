9214868aca82b7a00aaf1eaf38b2ad863d7172cb
dr-donut <m2hu@ucscd.edu>
2024-12-01 18:09:06 -0800

    bug fix; registration email does not enforce proper email format

--- client/src/components/Login/Signup.tsx
+++ client/src/components/Login/Signup.tsx
@@ -6,83 +6,97 @@ import Header from "../WorkOutPlan/Header";
 import "./css/Signup.css";
 
 function Signup() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [error, setError] = useState("");
-    const { setToken, setUser } = useAuth();
-    const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [error, setError] = useState("");
+	const { setToken, setUser } = useAuth();
+	const navigate = useNavigate();
 
-    async function handleSignup(event: any) {
-        event.preventDefault();
+	async function handleSignup(event: any) {
+		event.preventDefault();
 
-        if (!email || !password || !firstName || !lastName) {
-            setError("Please fill all fields");
-        }
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        try {
-            const data = await register(email, password, firstName, lastName);
+		if (!email || !password || !firstName || !lastName) {
+			setError("Please fill all fields");
+		}
 
-            if (data.result) {
-                setUser(email);
-                setToken(data.token);
-                localStorage.setItem("authToken", data.token);
-                navigate("/home");
-            } else {
-                setError(data.message);
-            }
-        } catch (error) {
-            setError("Signup failed. Please try again.");
-        }
-    }
+		if (!emailRegex.test(email)) {
+			setError("Please enter a valid email address");
+			return;
+		}
 
-    return (
-        <div className="register">
-            <Header />
-            <h1>Sign Up</h1>
-            <div className="back-to-login-button">
-                <button onClick={() => navigate("/login")}>Back to Login</button>
-            </div>
-            <form onSubmit={handleSignup}>
-                {error && <p style={{ color: "red" }}>{error}</p>}
-                <div className="form-group">
-                    <h2>Email:</h2>
-                    <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="form-group">
-                    <h2>Password:</h2>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <h2>First Name:</h2>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <h2>Last Name:</h2>
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </div>
-                <div className="submit">
-                    <button type="submit">Create Account</button>
-                </div>
-            </form>
-        </div>
-    );
+		try {
+			const data = await register(email, password, firstName, lastName);
+
+			if (data.result) {
+				setUser(email);
+				setToken(data.token);
+				localStorage.setItem("authToken", data.token);
+				navigate("/home");
+			} else {
+				setError(data.message);
+			}
+		} catch (error) {
+			setError("Signup failed. Please try again.");
+		}
+	}
+
+	return (
+		<div className="register">
+			<Header />
+			<h1>Sign Up</h1>
+			<div className="back-to-login-button">
+				<button onClick={() => navigate("/login")}>
+					Back to Login
+				</button>
+			</div>
+			<form onSubmit={handleSignup}>
+				{error && <p style={{ color: "red" }}>{error}</p>}
+				<div className="form-group">
+					<h2>Email:</h2>
+					<input
+						type="text"
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<h2>Password:</h2>
+					<input
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<h2>First Name:</h2>
+					<input
+						type="text"
+						placeholder="First Name"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<h2>Last Name:</h2>
+					<input
+						type="text"
+						placeholder="Last Name"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+				</div>
+				<div className="submit">
+					<button type="submit">Create Account</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Signup;
